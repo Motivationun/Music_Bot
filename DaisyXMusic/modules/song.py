eaf64a03e53da08ba1f0f279a1e421a41a15dad0
A N I K E T <86464839+Aniket-Code10010@users.noreply.github.com>
2021-08-21 21:24:34 +0530

    Update song.py

--- DaisyXMusic/modules/song.py
+++ DaisyXMusic/modules/song.py
@@ -53,7 +53,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 Finding the song...")
+    m = message.reply("🔎 Finding the song 🔎")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -70,7 +70,7 @@ def song(client, message):
         results[0]["views"]
 
     except Exception as e:
-        m.edit("❌ Found Nothing.\n\nTry another keywork or maybe spell it properly.")
+        m.edit("❌ Found Nothing ❌\n\nTry another keywork or maybe spell it properly.")
         print(str(e))
         return
     m.edit("Downloading the song ")
@@ -79,7 +79,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by **"
+        rep = "**🎵 Uploaded by 𝔸 N I Ꮶ E T 🎵**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
@@ -94,7 +94,7 @@ def song(client, message):
         )
         m.delete()
     except Exception as e:
-        m.edit("❌ Error")
+        m.edit("❌ Error ❌")
         print(e)
 
     try:
